35fdfb73ea28badfc23dcf8d1807ae562aaff50c
Krittimon Promsanar <krittimonp28@gmail.com>
2024-02-11 00:22:32 +0700

    add page in navbar

--- app.py
+++ app.py
@@ -6,5 +6,26 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/arsenal')
+def arsenal():
+    return render_template('arsenal.html')
+
+@app.route('/category')
+def category():
+    return render_template('category.html')
+    
+@app.route('/write')
+def write():
+    return render_template('write.html')
+
+@app.route('/discuss')
+def discuss():
+    return render_template('discuss.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
